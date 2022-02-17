3cb4e0f3ea596a2baa5f6dfb88b82287ddc31b80
lysiandon <don.lysiane@gmail.com>
2022-02-17 17:24:24 +0100

    created getStorage and getMovie methods

--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,10 +1,55 @@
 import React, { Component } from 'react'
 
 export default class Favorites extends Component {
+
+    constructor(){
+
+        super();
+
+        this.state = {
+            movies : [],
+            favIDs : this.getStorage(),
+        }
+
+        //Bind functions
+        this.getStorage = this.getStorage.bind(this);
+        this.getMovie = this.getMovie.bind(this);
+    }
+
+    getStorage(){
+
+        const favArray = localStorage.getItem("fav").split(",");
+        // console.log('favarray', favArray);
+        return favArray;
+    }
+
+    getMovie(id){
+
+        fetch(`http://api.themoviedb.org/3/movie/${id}?api_key=bcab3683c270f0eed6dc788a93c2e943`)
+        .then(res => res.json())
+        .then(res => {
+
+            const favoritemovie = res;
+            // const favmovie = favoritemovies.find(movieid => id)      
+            console.log("test fav movie");
+
+            const copyArray = this.state.movies;
+            copyArray.push(favoritemovie);
+
+            this.setState({
+                movies : copyArray,
+            })
+        })
+    }
+
+
     render() {
         return (
             <div>
-                <h1>Favorites</h1>
+                <h1>Favorites : {this.state.favIDs}</h1>
+                
+                {this.getStorage()}
+                {/* {this.getMovie()} */}
             </div>
         )
     }
